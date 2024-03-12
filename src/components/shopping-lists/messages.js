6c4a8e071f4cd7bbab0e24b69de918d7d115435f
pbukala-ct <piotr.bukala@commercetools.com>
2024-03-12 16:21:21 +1100

    rename to Gift Registry

--- src/components/shopping-lists/messages.js
+++ src/components/shopping-lists/messages.js
@@ -3,15 +3,15 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   title: {
     id: 'ShoppingLists.title',
-    defaultMessage: 'Shopping list',
+    defaultMessage: 'Gift Registry list',
   },
   subtitle: {
     id: 'ShoppingLists.subtitle',
-    defaultMessage: 'List of all shooping lists from this customer',
+    defaultMessage: 'List of all Gift Registries from this customer',
   },
   noResults: {
     id: 'ShoppingLists.noResults',
-    defaultMessage: 'There are no shopping lists available for this user',
+    defaultMessage: 'There are no Gift Registries available for this user',
   },
   actions: {
     id: 'ShoppingLists.actions',
